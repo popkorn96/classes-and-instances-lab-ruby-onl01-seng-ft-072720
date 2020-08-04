9a8397bc9f0ab230ba4f628e6dfe356457a452bb
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-04 18:35:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,3 +3,8 @@ class Person
 end
 
 adele_goldberg = Dog.new 
+
+
+Under your Person class definition, create two people in local variables, adele_goldberg and alan_kay
+
+When you're done, submit the lab with learn submit.
