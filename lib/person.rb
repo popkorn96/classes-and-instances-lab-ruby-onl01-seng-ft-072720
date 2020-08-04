cdbd0531c7510b08314a4f54f9076ede2d5b7fb5
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-04 18:37:54 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+  
+end
+
+adele_goldberg = Person.new 
+alan_kay = Person.new
+
+#Under your Person class definition, create two people in local variables, adele_goldberg and alan_kay
+
+#When you're done, submit the lab with learn submit.
